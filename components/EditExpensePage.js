4cf54379fae24e0ee39b0d27e13e230fd6b4ebca
naurinah <87181806+naurinah@users.noreply.github.com>
2021-07-29 18:16:15 +0500

    Update EditExpensePage.js

--- components/EditExpensePage.js
+++ components/EditExpensePage.js
@@ -36,7 +36,7 @@ export default function EditExpensePage () {
       };
     return (
          <div>
-          <Button variant="outlined" color="primary" onClick={handleClickOpen}></Button>
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>Action</Button>
       <Dialog
         open={open}
         onClose={handleClose}
