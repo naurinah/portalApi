f3a5c2c4da04ef94305ba44d702612fc8c6006a7
naurinah <87181806+naurinah@users.noreply.github.com>
2021-07-30 12:22:35 +0500

    Update Modals.js

--- components/Modals.js
+++ components/Modals.js
@@ -17,11 +17,6 @@ import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
 import Paper from "@material-ui/core/Paper";
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
@@ -182,107 +177,7 @@ export default function Modals({ show, onHide, acno }) {
   const [isLoading, setIsLoading] = React.useState(true);
   const ac = "";
   const [modalAcno, setModalAcno] = React.useState("");
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  function createData(name, cell, city, email) {
-  return {name, cell, city, email};
-}
-  function EditExpensePage () {
-        const classes = useStyles();
-     
-        const [apis, setApis] = React.useState(null);
-      
-       
-        const [rows, setRows] = React.useState([]);
-    
-     const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-    
-    
-    
-        const fetchAccountDetails = async (ac) => {
-       let newRows = rows;
-      const response = await fetch(
-        `http://portal.blue-ex.com/api1/customerportal/viewprofile.py?acno=${ac}`
-      ).then((res) => res.json());
-      };
  
-    return (
-         <div>
-        <Button variant="outlined" color="primary" onClick={handleClickOpen}>View Details</Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Customer Account Details"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-             <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Cell</TableCell>
-            <TableCell align="right">City</TableCell>
-            <TableCell align="right">Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">response.{row.Name}</TableCell>
-              <TableCell align="right">response.{row.Cell}</TableCell>
-              <TableCell align="right">response.{row.Email}</TableCell>
-              <TableCell align="right">response.{row.Email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-              
-          </DialogContentText>
-        </DialogContent>
-       <DialogActions>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-        </div>
-       
-    )
-}
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   const fetchAccountDetails = async (ac) => {
     let newRows = rows;
@@ -297,9 +192,7 @@ export default function Modals({ show, onHide, acno }) {
             a["acno"],
             a["total_Hits"],
             a["Last_Hit"],
-          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>  VIEW ACTION </Button>
            
           )
           ];
@@ -311,10 +204,7 @@ export default function Modals({ show, onHide, acno }) {
         a["acno"],
         a["total_Hits"],
         a["Last_Hit"],
-      
-     <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>  VIEW ACTION </Button>
       
       ),
     );
