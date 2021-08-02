93fc7e7f122afe4424b0450850f5d6f60f9aca61
naurinah <87181806+naurinah@users.noreply.github.com>
2021-08-02 12:07:54 +0500

    Update Modals.js

--- components/Modals.js
+++ components/Modals.js
@@ -316,7 +316,7 @@ export default function Modals({ show, onHide, acno }) {
                           <TableCell>{row.acno}</TableCell>
                           <TableCell>{row.total_Hits}</TableCell>
                           <TableCell>{row.Last_Hit}</TableCell>
-                             <TableCell> {row.action}</TableCell>
+                          <TableCell> {row.action}</TableCell>
                          </TableRow>
                       );
                     })}
