872e332267bd0d20bb89c4c7c58785a5949087d5
naurinah <87181806+naurinah@users.noreply.github.com>
2021-08-02 18:07:19 +0500

    Update EditExpensePage.js

--- components/EditExpensePage.js
+++ components/EditExpensePage.js
@@ -1,87 +1,365 @@
+import React from "react";
+import { Button } from "@material-ui/core";
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-const useStyles = makeStyles({
-  table: {
-    minWidth: 850,
-  },
-});
-function createData(name, cell, city, email) {
-  return {name, cell, city, email};
+import Modal from "react-bootstrap/Modal";
+import PropTypes from "prop-types";
+import { lighten, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import { TableSortLabel } from "@material-ui/core";
+// import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Paper from "@material-ui/core/Paper";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import Modals from "./Modals";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import SearchBar from "material-ui-search-bar";
+
+function createData(name, city, cell, email) {
+  return { name, city, cell, email };
 }
-export default function EditExpensePage () {
-        const classes = useStyles();
-        const handleClose = () => { setOpen(false); };
-        const [apis, setApis] = React.useState(null);
-        
-        const fetchAccountDetails = async (ac) => {
-       let newRows = rows;
-      const response = await fetch(
-        `http://portal.blue-ex.com/api1/customerportal/viewprofile.py?acno=${ac}`
-      ).then((res) => res.json());
-      };
- 
-    return (
-         <div>
-         <Button variant="outlined" color="primary" onClick={handleClickOpen}>  VIEW ACTION </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  { id: "name", numeric: false, disablePadding: false, label: "API NAME" },
+  { id: "city", numeric: false, disablePadding: false, label: "CITY" },
+  { id: "cell", numeric: true, disablePadding: false, label: "CELL" },
+  { id: "email", numeric: false, disablePadding: false, label: "EMAIL" },
+];
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, rowCount, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead className="bg-[#f5f5fd]">
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+/*EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+ onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};*/
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === "light"
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: "1 1 100%",
+  },
+}));
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  paper: {
+    width: "100%",
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
+  },
+}));
+
+let no = 0;
+export default function EditExpensePage({
+  reload,
+  setReload,
+  show,
+  onHide,
+  acno,
+}) {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("calories");
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [modalShow, setModalShow] = React.useState(false);
+  const [modalAcno, setModalAcno] = React.useState("");
+  const [apis, setApis] = React.useState(null);
+  const [rows, setRows] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const fetchEditExpensePage = async () => {
+    const response = await fetch(
+      "https://bigazure.com/api/json_v4/dashboard/API_PORTAL_API/api_detail.php"
+    ).then((res) => res.json());
+    setApis(response);
+  };
+
+  const [originalRows, setOriginalRows] = React.useState([]);
+  const [searched, setSearched] = React.useState("");
+
+  const requestSearch = (searchedVal) => {
+    const filteredRowsNo = originalRows.filter((row) => {
+      return row.no.toLowerCase().includes(searchedVal.toLowerCase());
+    });
+    const filteredRowsName = originalRows.filter((row) => {
+      return row.name.toLowerCase().includes(searchedVal.toLowerCase());
+    });
+    //console.log(filteredRowsNo);
+    setRows([...new Set([...filteredRowsNo, ...filteredRowsName])]);
+  };
+  const cancelSearch = () => {
+    setSearched("");
+    requestSearch(searched);
+  };
+
+  React.useEffect(async () => {
+    if (reload) {
+      setIsLoading(true);
+      await fetchEditExpensePage();
+      setIsLoading(false);
+      setReload(false);
+    }
+  }, [reload]);
+
+  React.useEffect(() => {
+    if (apis) {
+      setOriginalRows([]);
+      let newRows = [];
+      apis.map((a) => {
+        newRows.push(
+          createData(a["api_name"], a["api_cell"], a["city"], a["email"])
+        );
+      });
+      setRows(newRows);
+      setOriginalRows(newRows);
+      setIsLoading(false);
+    }
+  }, [apis]);
+
+  React.useEffect(async () => {
+    await fetchEditExpensePage();
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <div>
+      <Modal
+        show={show}
+        onHide={onHide}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
       >
-        <DialogTitle id="alert-dialog-title">{"Customer Account Details"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-             <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Cell</TableCell>
-            <TableCell align="right">City</TableCell>
-            <TableCell align="right">Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">response.{row.Name}</TableCell>
-              <TableCell align="right">response.{row.Cell}</TableCell>
-              <TableCell align="right">response.{row.Email}</TableCell>
-              <TableCell align="right">response.{row.Email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-              
-          </DialogContentText>
-        </DialogContent>
-       <DialogActions>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-        </div>
-       
-    )
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Customer Related to this API
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <CircularProgress />
+            </div>
+          ) : (
+            <Paper className={classes.paper}>
+              <div className="flex justify-between items-center mb-[1rem]">
+                <SearchBar
+                  value={searched}
+                  onChange={(searchVal) => requestSearch(searchVal)}
+                  onCancelSearch={() => cancelSearch()}
+                />
+              </div>
+              <TableContainer>
+                <Table
+                  className={classes.table}
+                  aria-labelledby="tableTitle"
+                  size={"medium"}
+                  aria-label="enhanced table"
+                >
+                  <EnhancedTableHead
+                    classes={classes}
+                    order={order}
+                    orderBy={orderBy}
+                    onRequestSort={handleRequestSort}
+                    rowCount={rows.length}
+                  />
+                  <TableBody>
+                    {stableSort(rows, getComparator(order, orderBy))
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((row, index) => {
+                        const labelId = `enhanced-table-checkbox-${index}`;
+
+                        return (
+                          <TableRow hover tabIndex={-1} key={row.no}>
+                            <TableCell>{row.name}</TableCell>
+                            <TableCell>{row.city}</TableCell>
+                            <TableCell>{row.cell}</TableCell>
+                            <TableCell>{row.email}</TableCell>
+                          </TableRow>
+                        );
+                      })}
+                    {emptyRows > 0 && (
+                      <TableRow style={{ height: 53 * emptyRows }}>
+                        <TableCell colSpan={6} />
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[25, 50, 100, 150]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+              />
+            </Paper>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
