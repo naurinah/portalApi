3aefef2b7f7618880810067d9f805749a4997cdb
naurinah <87181806+naurinah@users.noreply.github.com>
2021-08-03 10:52:19 +0500

    Update EditExpensePage.js

--- components/EditExpensePage.js
+++ components/EditExpensePage.js
@@ -364,23 +364,6 @@ const response = await fetch(
   );
 }
 
-import Modal from "react-bootstrap/Modal";
-import PropTypes from "prop-types";
-import { lighten, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import { TableSortLabel } from "@material-ui/core";
-// import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Paper from "@material-ui/core/Paper";
-import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
-import Modals from "./Modals";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import SearchBar from "material-ui-search-bar";
 
 function createData(Name, Address, Cell, Email) {
   return { Name, Address, Cell, Email };
